2c32c30fb62eb80cca8d0ba2e39d27028efd9cd0
Nacho Barrientos <nacho.barrientos@cern.ch>
2018-01-23 15:08:20 +0100

    Remove unused script

--- install_foreman.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Description:  Runs the foreman installer as a separate process so we don't
-#               have to worry about a nonzero exit status (which we don't care
-#               about most of the time).
-#
-
-/usr/sbin/foreman-installer
-exit 0
